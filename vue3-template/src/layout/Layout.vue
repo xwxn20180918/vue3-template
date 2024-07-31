3928631a5847ab8c414400137ae3b9a408accbbf
xiongwen <xiongwen@zhonglian.com>
2024-07-31 20:26:48 +0800

    chore: 项目目录结构

---
+++ vue3-template/src/layout/Layout.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>hello</h1>
+</template>
+
+<script setup lang="ts">
+import {computed, onMounted, watch} from 'vue';
+
+computed(() => {
+
+});
+
+onMounted(() => {
+
+});
+</script>
+
+<style lang="less" scoped>
+
+</style>
